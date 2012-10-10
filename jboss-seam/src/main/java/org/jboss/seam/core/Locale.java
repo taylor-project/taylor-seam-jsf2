21eb9b2544b651e49acada18290eea25f766caf6
mikedon <mikedonovan1987@gmail.com>
2012-10-10 19:07:43 -0400

    locale is now event scoped

--- jboss-seam/src/main/java/org/jboss/seam/core/Locale.java
+++ jboss-seam/src/main/java/org/jboss/seam/core/Locale.java
@@ -4,42 +4,44 @@ import static org.jboss.seam.annotations.Install.BUILT_IN;
 
 import org.jboss.seam.Component;
 import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Unwrap;
-import org.jboss.seam.annotations.intercept.BypassInterceptors;
 import org.jboss.seam.contexts.Contexts;
 
 /**
- * Manager component for the current locale. This base
- * implementation simply returns the server default 
- * locale.
+ * Manager component for the current locale. This base implementation simply
+ * returns the server default locale.
  * 
  * @author Gavin King
  */
-@Scope(ScopeType.STATELESS)
+@Scope(ScopeType.EVENT)
 @Name("org.jboss.seam.core.locale")
-@Install(precedence=BUILT_IN)
-@BypassInterceptors
-public class Locale 
-{
+@Install(precedence = BUILT_IN)
+@AutoCreate
+//@BypassInterceptors
+public class Locale {
 
-   @Unwrap
-   public java.util.Locale getLocale()
-   {
-      return java.util.Locale.getDefault();
-   }
-     
-   public static java.util.Locale instance()
-   {
-       if ( Contexts.isApplicationContextActive() ) 
-       {
-          return (java.util.Locale) Component.getInstance(Locale.class, ScopeType.STATELESS);
-       } 
-       else 
-       {
-          return java.util.Locale.getDefault(); //for unit tests
-       }
-   }
+	protected java.util.Locale locale;
+	
+	@Create
+	public void create(){
+		this.locale = java.util.Locale.getDefault();
+	}
+	
+	@Unwrap
+	public java.util.Locale getLocale() {
+		return locale;
+	}
+	
+	public static java.util.Locale instance() {
+		if (Contexts.isApplicationContextActive()) {
+			return (java.util.Locale) Component.getInstance(Locale.class);
+		} else {
+			return java.util.Locale.getDefault(); // for unit tests
+		}
+	}
 }
--- jboss-seam/src/main/java/org/jboss/seam/international/Locale.java
+++ jboss-seam/src/main/java/org/jboss/seam/international/Locale.java
@@ -3,32 +3,34 @@ package org.jboss.seam.international;
 import static org.jboss.seam.annotations.Install.FRAMEWORK;
 
 import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.annotations.Unwrap;
 import org.jboss.seam.annotations.intercept.BypassInterceptors;
 import org.jboss.seam.contexts.Contexts;
 
 /**
- * Manager component for the current locale that is
- * aware of the selected locale
+ * Manager component for the current locale that is aware of the selected locale
  * 
  * @author Gavin King
  */
-@Scope(ScopeType.STATELESS)
+@Scope(ScopeType.EVENT)
 @Name("org.jboss.seam.core.locale")
-@Install(precedence=FRAMEWORK, dependencies="org.jboss.seam.international.localeSelector")
-@BypassInterceptors
-public class Locale extends org.jboss.seam.core.Locale
-{
+@Install(precedence = FRAMEWORK, dependencies = "org.jboss.seam.international.localeSelector")
+//@BypassInterceptors
+@AutoCreate
+public class Locale extends org.jboss.seam.core.Locale {
 
-   @Unwrap @Override
-   public java.util.Locale getLocale()
-   {
-      return Contexts.isSessionContextActive() ?
-            LocaleSelector.instance().getLocale() :
-            super.getLocale();
-   }
-   
+	@Create
+	@Override
+	public void create() {
+		if(Contexts.isSessionContextActive()){
+			this.locale = LocaleSelector.instance().getLocale();
+		}else{
+			super.create();
+		}
+	}
+
 }
