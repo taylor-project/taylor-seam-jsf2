fd59c6ddfb0ffef0452bbe746c88b876fd6e7a2d
eddie--lau <JamaicaBlueMountain@yahoo.com>
2014-10-28 09:11:27 -0400

    Removing the 'rules' and 'facts' in RuleBasedPermissionResolver from the session

---
+++ jboss-seam/src/main/java/org/jboss/seam/security/permission/RuleBaseFactory.java
@@ -0,0 +1,72 @@
+package org.jboss.seam.security.permission;
+
+import org.drools.RuleBase;
+import org.drools.StatefulSession;
+import org.jboss.seam.Component;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Factory;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.Startup;
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
+import org.jboss.seam.contexts.Contexts;
+import org.jboss.seam.drools.SeamGlobalResolver;
+import org.jboss.seam.security.Identity;
+import org.jboss.seam.security.management.JpaIdentityStore;
+
+@Name("ruleBaseFactory")
+@Scope(ScopeType.APPLICATION)
+@BypassInterceptors
+@Startup
+public class RuleBaseFactory {
+
+	private static RuleBase securityRuleBase;
+
+	public static RuleBaseFactory instance() {
+		return (RuleBaseFactory) Component.getInstance("ruleBaseFactory", true);
+	}
+
+	@Factory(value = "securityRuleBase", scope = ScopeType.STATELESS, autoCreate = true)
+	public RuleBase getSecurityRuleBase() {
+		if (securityRuleBase == null) {
+			setSecurityRuleBase((RuleBase) Component.getInstance(
+					RuleBasedPermissionResolver.RULES_COMPONENT_NAME, true));
+		}
+
+		return securityRuleBase;
+	}
+
+	public void setSecurityRuleBase(RuleBase securityRuleBase) {
+		this.securityRuleBase = securityRuleBase;
+	}
+
+	@Factory(value = "securityContext", scope = ScopeType.EVENT, autoCreate = true)
+	public StatefulSession getSecurityContext() {
+		RuleBase sRules;
+		StatefulSession sContext;
+		Identity identity;
+		
+		sRules = (RuleBase) Component.getInstance("securityRules");
+		if (sRules == null) {
+			return null;
+		}
+
+		sContext = sRules.newStatefulSession(true);
+		sContext.setGlobalResolver(new SeamGlobalResolver(sContext
+				.getGlobalResolver()));
+		
+		identity = Identity.instance();
+		if (identity != null) {
+			sContext.insert(identity.getPrincipal());
+
+			// If we were authenticated with the JpaIdentityStore, then insert the authenticated
+			// UserAccount into the security context.
+			if (Contexts.isEventContextActive() && Contexts.isSessionContextActive() &&
+				       Contexts.getEventContext().isSet(JpaIdentityStore.AUTHENTICATED_USER)) {
+				sContext.insert(Contexts.getEventContext().get(JpaIdentityStore.AUTHENTICATED_USER));
+			}
+		}
+
+		return sContext;
+	}
+}
