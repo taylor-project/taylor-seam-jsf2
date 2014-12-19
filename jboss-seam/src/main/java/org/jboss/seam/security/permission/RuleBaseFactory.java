d4adfc25f22b0b666aaafc152d2fe9999b5d69dc
mikedon <mikedonovan1987@gmail.com>
2014-12-19 17:41:23 -0500

    moving securityContext to a standalone component so that we can call dispose on the stateful session

--- jboss-seam/src/main/java/org/jboss/seam/security/permission/RuleBaseFactory.java
+++ jboss-seam/src/main/java/org/jboss/seam/security/permission/RuleBaseFactory.java
@@ -1,7 +1,6 @@
 package org.jboss.seam.security.permission;
 
 import org.drools.RuleBase;
-import org.drools.StatefulSession;
 import org.jboss.seam.Component;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Factory;
@@ -10,10 +9,6 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Startup;
 import org.jboss.seam.annotations.intercept.BypassInterceptors;
-import org.jboss.seam.contexts.Contexts;
-import org.jboss.seam.drools.SeamGlobalResolver;
-import org.jboss.seam.security.Identity;
-import org.jboss.seam.security.management.JpaIdentityStore;
 
 @Name("ruleBaseFactory")
 @Scope(ScopeType.APPLICATION)
@@ -40,35 +35,5 @@ public class RuleBaseFactory {
 
 	public void setSecurityRuleBase(RuleBase securityRuleBase) {
 		this.securityRuleBase = securityRuleBase;
-	}
-
-	@Factory(value = "securityContext", scope = ScopeType.EVENT, autoCreate = true)
-	public StatefulSession getSecurityContext() {
-		RuleBase sRules;
-		StatefulSession sContext;
-		Identity identity;
-		
-		sRules = (RuleBase) Component.getInstance("securityRules");
-		if (sRules == null) {
-			return null;
-		}
-
-		sContext = sRules.newStatefulSession(true);
-		sContext.setGlobalResolver(new SeamGlobalResolver(sContext
-				.getGlobalResolver()));
-		
-		identity = Identity.instance();
-		if (identity != null) {
-			sContext.insert(identity.getPrincipal());
-
-			// If we were authenticated with the JpaIdentityStore, then insert the authenticated
-			// UserAccount into the security context.
-			if (Contexts.isEventContextActive() && Contexts.isSessionContextActive() &&
-				       Contexts.getEventContext().isSet(JpaIdentityStore.AUTHENTICATED_USER)) {
-				sContext.insert(Contexts.getEventContext().get(JpaIdentityStore.AUTHENTICATED_USER));
-			}
-		}
-
-		return sContext;
 	}
 }
---
+++ jboss-seam/src/main/java/org/jboss/seam/security/permission/SecurityContext.java
@@ -0,0 +1,64 @@
+package org.jboss.seam.security.permission;
+
+import org.drools.RuleBase;
+import org.drools.StatefulSession;
+import org.jboss.seam.Component;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Destroy;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.Unwrap;
+import org.jboss.seam.contexts.Contexts;
+import org.jboss.seam.drools.SeamGlobalResolver;
+import org.jboss.seam.security.Identity;
+import org.jboss.seam.security.management.JpaIdentityStore;
+
+@Name("securityContext")
+@Scope(ScopeType.EVENT)
+@AutoCreate
+public class SecurityContext {
+
+	private StatefulSession sContext;
+
+	@Unwrap
+	public StatefulSession getSecurityContext() {
+		if (sContext == null) {
+			RuleBase sRules;
+			Identity identity;
+
+			sRules = (RuleBase) Component.getInstance("securityRules");
+			if (sRules == null) {
+				return null;
+			}
+
+			sContext = sRules.newStatefulSession(true);
+			sContext.setGlobalResolver(new SeamGlobalResolver(sContext
+					.getGlobalResolver()));
+
+			identity = Identity.instance();
+			if (identity != null) {
+				sContext.insert(identity.getPrincipal());
+
+				// If we were authenticated with the JpaIdentityStore, then
+				// insert the authenticated
+				// UserAccount into the security context.
+				if (Contexts.isEventContextActive()
+						&& Contexts.isSessionContextActive()
+						&& Contexts.getEventContext().isSet(
+								JpaIdentityStore.AUTHENTICATED_USER)) {
+					sContext.insert(Contexts.getEventContext().get(
+							JpaIdentityStore.AUTHENTICATED_USER));
+				}
+			}
+		}
+		return sContext;
+	}
+
+	@Destroy
+	public void destroy() {
+		if (sContext != null) {
+			sContext.dispose();
+		}
+	}
+}
